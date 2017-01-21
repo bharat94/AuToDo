c996670f15a7b2c1341e5dfc10671baaa9381418
Bharat V <vaidhyanathan.b@husky.neu.edu>
2017-01-21 12:55:42 -0500

    Landing page done with just the UI

--- app/src/main/java/com/spartahack/autodo/MainActivity.java
+++ app/src/main/java/com/spartahack/autodo/MainActivity.java
@@ -2,6 +2,8 @@ package com.spartahack.autodo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
     }
 }
