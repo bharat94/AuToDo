81a74c41afa4224b5118a95566d07533436bb500
Bharat V <vaidhyanathan.b@husky.neu.edu>
2017-01-21 16:35:23 -0500

    Added read contacts permission to the manifest

--- app/src/main/java/com/spartahack/autodo/MainActivity.java
+++ app/src/main/java/com/spartahack/autodo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.provider.ContactsContract;
 
 import com.evernote.client.android.EvernoteSession;
 
